2d60dcd3f04c3ecc578b066795812ad80672a34f
Dorian94 <dorian.wallez@gmail.com>
2020-03-12 16:01:23 +0000

    commit

---
+++ app.js
@@ -0,0 +1,58 @@
+/*
+Dorian
+WALLEZ
+22490
+*/
+
+/* 3. 7*/
+
+/* 4. */ function myAlphabatLength(arr)
+{
+        console.log(arr.length);
+}
+
+/* 5. */ var Planets = ["Saturne", "Mars", "Earth", "Venus", "Jupiter"];
+
+/* 6.   */
+for (let index = 0; index < Planets.length; index++) {
+    console.log(Planets[index]);
+    
+}
+
+/* 7. */ var wowDatatypes = [1, "2", []];
+
+wowDatatypes.forEach(element => {
+    console.log(element);
+    
+});
+
+var a, b, c;
+a = 5;
+
+b = 10;
+c = "5";
+var x = a;
+
+
+/*
+
+if(a <= c){
+console.log("equal");
+} 
+
+if(a >= c){
+   console.log("identical");
+}
+
+*/
+
+/*  11  */ 
+
+var hello =  function SayHello() {
+    return "Hello World!";
+}
+
+
+/*  12.  */
+
+console.log(hello);
